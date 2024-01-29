cb8567836534f53248f51a7c611d604bb33886f0
anupamhaldkar <anupam.haldkar@outlook.com>
2024-01-29 23:13:44 +0530

    feature: Employee List table tailwind

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -6,6 +6,43 @@ const EmployeeList = () => {
     <div className="h-12">
         <button className='rounded bg-slate-700 text-white px-6 py-2 font-semibold'> Add Employee</button>
     </div>
+    <div className='flex shadow border-b'>
+        <table className='min-w-full'>
+            <thead className='bg-gray-50'>
+                <th className='text-left font-medium text-gray-500 uppercase tracking-wider px-6 py-3'>
+                    First Name
+                </th>
+                <th className='text-left font-medium text-gray-500 uppercase tracking-wider px-6 py-3'>
+                    Last Name
+                </th>
+                <th className='text-left font-medium text-gray-500 uppercase tracking-wider px-6 py-3'>
+                    Email Id
+                </th>
+                <th className='text-right font-medium text-gray-500 uppercase tracking-wider px-6 py-3'>
+                    Actions
+                </th>
+            </thead>
+            <tbody className='bg-white'>
+                <tr>
+                    <td className='text-left px-6 py-4 whitespace-nowrap'>
+                        <div className='text-sm text-gray-500'>A</div>
+                    </td>
+                    <td className='text-left px-6 py-4 whitespace-nowrap'>
+                        <div className='text-sm text-gray-500'>A</div>
+                    </td>
+                    <td className='text-left px-6 py-4 whitespace-nowrap'>
+                        <div className='text-sm text-gray-500'>A@gmail.com</div>
+                    </td>
+                    <td className='text-right px-6 py-4 whitespace-nowrap font-medium text-sm'>
+                        <a href="#" className='text-indigo-500 hover:text-indigo-600 px-4'>Edit</a>
+                        <a href="#" className='text-indigo-500 hover:text-indigo-600 px-4'>Delete</a>
+                    </td>
+                
+                </tr>
+            </tbody>
+        </table>
+        
+    </div>
     </div>
   )
 };

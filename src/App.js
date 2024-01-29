99bbaf6cd01809a9db7bd58c641966515bcda3bd
anupamhaldkar <anupam.haldkar@outlook.com>
2024-01-29 22:42:58 +0530

    feature: react router added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import AddEmployee from './components/AddEmployee';
 import Navbar from './components/Navbar';
+import EmployeeList from './components/EmployeeList';
 
 function App() {
   return (
     <>
-   <Navbar /> 
-   <AddEmployee />
+    <BrowserRouter>
+      <Navbar /> 
+      <Routes>
+        <Route path="/" element={<EmployeeList />}></Route>
+        <Route index element={<EmployeeList />} />
+        <Route path="/employeeList" element={<EmployeeList />} />
+        <Route path="/addEmployee" element={<AddEmployee />} />
+      </Routes>
+    </BrowserRouter>
+   
    </>
   );
 }

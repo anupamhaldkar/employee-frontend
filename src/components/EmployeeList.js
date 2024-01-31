6f5d13c46dd631ad740600335da8f8ecb3b5ab56
anupamhaldkar <anupam.haldkar@outlook.com>
2024-01-31 10:15:12 +0530

    refactor: added employee component

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 import EmployeeService from '../services/EmployeeService';
+import Employee from './Employee';
 
 const EmployeeList = () => {
 
@@ -54,27 +55,7 @@ const EmployeeList = () => {
             {!loading && (
             <tbody className='bg-white'>
                 {employees.map((employee)=>(
-                <tr key={employee.id}>
-                    <td className='text-left px-6 py-4 whitespace-nowrap'>
-                        <div className='text-sm text-gray-500'>
-                            {employee.firstName}
-                        </div>
-                    </td>
-                    <td className='text-left px-6 py-4 whitespace-nowrap'>
-                        <div className='text-sm text-gray-500'>
-                            {employee.lastName}
-                        </div>
-                    </td>
-                    <td className='text-left px-6 py-4 whitespace-nowrap'>
-                        <div className='text-sm text-gray-500'>
-                            {employee.emailId}
-                        </div>
-                    </td>
-                    <td className='text-right px-6 py-4 whitespace-nowrap font-medium text-sm'>
-                        <a href="#" className='text-indigo-500 hover:text-indigo-600 px-4'>Edit</a>
-                        <a href="#" className='text-indigo-500 hover:text-indigo-600 px-4'>Delete</a>
-                    </td>
-                </tr>
+                <Employee employee={employee} key={employee.id}></Employee>
                 ))}
             </tbody>
             )}

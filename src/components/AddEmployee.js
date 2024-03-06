889e7695a8e56afa2ac302e97390cdf3dc0cdff7
anupamhaldkar <anupam.haldkar@outlook.com>
2024-03-06 23:47:27 +0530

    feature - added form and tailwind

---
+++ src/components/AddEmployee.js
@@ -0,0 +1,37 @@
+import React from 'react'
+
+const AddEmployee = () => {
+  return (
+    <div className="flex max-w-2xl shadow mx-auto border-b">
+        <div className="px-8 py-8">
+            <div className="font-thin text-2xl tracking-wider">
+                <h1>Add Employee</h1>
+            </div>
+            <div className="items-center justify-center h-14 w-full my-6">
+                <label className="block text-gray-600 font-normal text-m">First Name</label>
+                <input type="text"
+                        className="h-10 w-96 border mt-2 px-2 py-2"></input>
+            </div>
+            <div className="items-center justify-center h-14 w-full my-6">
+                <label className="block text-gray-600 font-normal text-m">
+                    Last Name
+                </label>
+                <input type="text"
+                        className="h-10 w-96 border mt-2 px-2 py-2"></input>
+            </div>
+            <div className="items-center justify-center h-14 w-full my-6">
+                <label className="block text-gray-600 font-normal text-m">Email</label>
+                <input type="email"
+                        className="h-10 w-96 border mt-2 px-2 py-2"></input>
+            </div>
+            <div className="items-center justify-center h-14 w-full my-4 space-x-4 pt-4">
+                <button className="rounded font-semibold text-white my-2 bg-blue-400 hover:bg-blue-700 py-2 px-6">Save</button>
+                <button className="rounded font-semibold text-white my-2 bg-red-400 hover:bg-red-700 py-2 px-6">Reset</button>
+
+            </div>
+        </div> 
+    </div>
+  )
+}
+
+export default AddEmployee
